975ab578cde820bcf4f34b4b776cd9ceea79d8c9
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-11-14 16:16:25 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,11 +48,11 @@ import styles from "@site/src/pages/styles.module.css";
 const TITLE = translate({
   id: 'homepage.title',
   message:
-    "AiShort(ChatGPT Shortcut)-简单易用的 AI 快捷指令表，让生产力倍增！",
+  "AI Prompt(ChatGPT 提示词)-简单易用的 AI 快捷指令表，让生产力倍增！",
 });
 const DESCRIPTION = translate({
   id: 'homepage.description',
-  message: "AI Short 是一款用于管理和分享 AI 提示词的工具，帮助用户更有效地定制、保存和共享自己的提示词，以提高生产力。该平台还包括一个提示词分享社区，让用户轻松找到适用于不同场景的指令。",
+  message: "AI Prompt 是一款用于管理和分享 AI Prompt的工具，帮助用户更有效地定制、保存和共享自己的提示词，以提高生产力。该平台还包括一个提示词分享社区，让用户轻松找到适用于不同场景的指令。",
 });
 const SLOGAN = translate({
   id: 'homepage.slogan',
